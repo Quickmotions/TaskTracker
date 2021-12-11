3d9ec68966587f68fcf8a096e7cb4a49ca22d53f
Fergus <fergusbhaak@gmail.com>
2021-12-11 16:10:58 +0000

    Initial commit

---
+++ UI.py
@@ -0,0 +1,47 @@
+import sys
+from functools import partial
+
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QGridLayout
+from PyQt5.QtWidgets import QLineEdit
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QVBoxLayout
+
+
+class TrackerUI(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('To-Do Tracker')
+        self.setFixedSize(250, 250)
+        self.task_manager = Tasks()
+
+    def list_tasks(self):
+        for task in self.task_manager.tasks:
+            ...
+
+
+class Tasks:
+    def __init__(self):
+        self.tasks = []
+        self.load_tasks()
+
+    def load_tasks(self):
+        with open('tasks.data') as f:
+            for line in f.readlines():
+                self.tasks.append(line.strip())
+
+    def create_task(self):
+        ...
+
+
+def main():
+    app = QApplication(sys.argv)
+    ui = TrackerUI()
+    ui.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
